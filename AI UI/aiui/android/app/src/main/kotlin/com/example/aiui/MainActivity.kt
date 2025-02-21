c116c8285a6366da503a62f54d3dc8b487823a89
Rodney Mbasu <rodneymbasu@gmail.com>
2025-02-21 13:16:45 +0300

    Update UI

---
+++ AI UI/aiui/android/app/src/main/kotlin/com/example/aiui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aiui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- AI UI/learning_management_ai/android/app/src/main/kotlin/com/example/learning_management_ai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.learning_management_ai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
